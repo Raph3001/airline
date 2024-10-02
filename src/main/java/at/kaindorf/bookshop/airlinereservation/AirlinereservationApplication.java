ec0cf9c7255a680c4d20919a64d6e7fd95d2b54b
Rapha <rapha.bader@gmail.com>
2024-10-02 13:29:19 +0200

    initial commit

---
+++ src/main/java/at/kaindorf/bookshop/airlinereservation/AirlinereservationApplication.java
@@ -0,0 +1,13 @@
+package at.kaindorf.bookshop.airlinereservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinereservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirlinereservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/kaindorf/bookshop/airlinereservation/AirlinereservationApplicationTests.java
@@ -0,0 +1,13 @@
+package at.kaindorf.bookshop.airlinereservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlinereservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
